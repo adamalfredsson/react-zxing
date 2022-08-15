d1882a6835ae2c6db2b5c522f7f9ccf58c4f57da
Adam Alfredsson <alfredsson.adam@gmail.com>
2022-08-15 09:24:02 +0200

    Starter implementation of useZxing

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { useZxing } from "./useZxing";
---
+++ src/useZxing.ts
@@ -0,0 +1,48 @@
+import {
+  BrowserMultiFormatReader,
+  DecodeHintType,
+  Result,
+} from "@zxing/library";
+import { useEffect, useMemo, useRef } from "react";
+
+interface UseZxingOptions {
+  hints?: Map<DecodeHintType, any>;
+  constraints?: MediaStreamConstraints;
+  timeBetweenDecodingAttempts?: number;
+  onResult?: (result: Result) => void;
+  onError?: (error: Error) => void;
+}
+
+export const useZxing = ({
+  constraints = {
+    audio: false,
+    video: {
+      facingMode: "environment",
+    },
+  },
+  hints,
+  timeBetweenDecodingAttempts = 300,
+  onResult = () => {},
+  onError = () => {},
+}: UseZxingOptions = {}) => {
+  const ref = useRef<HTMLVideoElement>(null);
+
+  const reader = useMemo<BrowserMultiFormatReader>(() => {
+    const instance = new BrowserMultiFormatReader(hints);
+    instance.timeBetweenDecodingAttempts = timeBetweenDecodingAttempts;
+    return instance;
+  }, [hints, timeBetweenDecodingAttempts]);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    reader.decodeFromConstraints(constraints, ref.current, (result, error) => {
+      if (result) onResult(result);
+      if (error) onError(error);
+    });
+    return () => {
+      reader.reset();
+    };
+  }, [ref, reader]);
+
+  return { ref };
+};
