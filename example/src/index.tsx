fcff6408803ad4ee2149db468a8b756f5c1b6fef
Adam Alfredsson <alfredsson.adam@gmail.com>
2022-08-15 13:57:30 +0200

    Example use parcel instead of cra

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 
 root.render(
   <React.StrictMode>
