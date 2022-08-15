0dda2ef54facc936c98dcd6a933010231e600466
Adam Alfredsson <alfredsson.adam@gmail.com>
2022-08-15 13:39:42 +0200

    Implement useZxing in example

--- example/src/BarcodeScanner.tsx
+++ example/src/BarcodeScanner.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import { useZxing } from "react-zxing";
 
 export const BarcodeScanner: React.FC<{}> = () => {
-  const { ref } = useZxing();
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onResult(result) {
+      setResult(result.getText());
+    },
+  });
 
-  return <video ref={ref} />;
+  return (
+    <>
+      <video ref={ref} />
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
+    </>
+  );
 };
